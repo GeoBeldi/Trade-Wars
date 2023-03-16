81a8c9568aad2ff232a68d7bec3643ce54c6122e
Ben Cruise <ben_cruise@hotmail.co.uk>
2023-03-16 19:28:50 +0000

    api logic and putting HTML onto page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,62 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios, * as others from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-  
+const [namesArray, setNamesArray] = useState([]);
+const [priceArray, setPriceArray] = useState([]);
 
-const [coinData, setCoinData] = useState();
-
-// Make a request for a user with a given ID
-axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+// Get for Names
+useEffect(() => {
+  axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
   .then(function (response) {
-    // handle success
-    setCoinData(response);
-    console.log(coinData);
+    // handle success    
+    for (let a = 0; a < response.data.length; a++) {
+      setNamesArray(namesArray => [...namesArray, response.data[a].id]);
+    }    
   })
   .catch(function (error) {
     // handle error
     console.log(error);
-  })
-  .finally(function () {
-    // always executed
   });
+},[]);
+
+//Get for price
+useEffect(() => {
+  axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+  .then(function (response) {
+    // handle success    
+    for (let a = 0; a < response.data.length; a++) {
+      setPriceArray(priceArray => [...priceArray, response.data[a].current_price]);
+    }     
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+},[]);
 
   return (
-    <div className="App">
-      
+    <div className="App col-12">
+      <div className="main-title">
+        <span >Bitcoin Prices</span>
+      </div>
+      <div className="table">
+        <div className="names-div col-6">
+            <span className="title">Name</span>
+          {namesArray.slice(0, 10).map(name => 
+            <span>{name}</span>
+          )}
+        </div>
+        <div className="prices-div col-6">
+          <span className="title">Price</span>
+          {priceArray.slice(0, 10).map(price => 
+            <span>{price}</span>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
